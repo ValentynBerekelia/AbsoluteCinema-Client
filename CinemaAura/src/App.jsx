44376213016dbee96bb4625e5e494488b9b00984
Mykhailo Hordei <myhailo.gordei@gmail.com>
2026-01-19 15:06:31 +0200

    create basic structure. Added Inter fonts

---
+++ CinemaAura/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <p>asdasdasdas AHSHJJHD hello</p>
+    </>
+  )
+}
+
+export default App
