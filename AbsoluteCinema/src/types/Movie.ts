652499948a5ce6062e5b9a7643b275efcabc4878
Oleh Ponziuk <kuznicovoleh4@icloud.com>
2026-01-28 23:31:13 +0200

    Added map Movie from api

--- AbsoluteCinema/src/types/Movie.ts
+++ AbsoluteCinema/src/types/Movie.ts
@@ -54,3 +54,35 @@ export interface MovieRecommendation {
     title: string;
     poster: string;
 };
+
+export const mapMovieFromApi = (data: any): any[] => {
+    const movies = Array.isArray(data)
+        ? data
+        : data.movies ?? [data];
+
+    return movies.map((movie: any) => {
+        let movieId = movie.id;
+
+        if (typeof movieId === 'object' && movieId !== null && movieId.id) {
+            movieId = movieId.id;
+        }
+
+        return {
+            id: String(movieId),
+            title: movie.name,
+            image: movie.posterUrl ?? '',
+            genre: movie.genres?.join(', ') ?? '',
+            duration: parseDuration(movie.duration),
+            director: '',
+            starring: '',
+            ageLimit: movie.ageLimit,
+            format: '3D',
+            sessions: movie.sessionTimes?.map(convertIsoToDateTime) ?? []
+        };
+    });
+};
+
+const parseDuration = (durationStr: string): number => {
+    const [h, m, s] = durationStr.split(':').map(Number);
+    return h * 60 + m + s / 60;
+};
