38c746f51811d3ffe5aeb14d010ad5a807b1640f
Oleh Ponziuk <kuznicovoleh4@icloud.com>
2026-01-29 14:35:06 +0200

    Add get movies

--- AbsoluteCinema/src/pages/MovieDetails/MovieDetailsPage.tsx
+++ AbsoluteCinema/src/pages/MovieDetails/MovieDetailsPage.tsx
@@ -1,16 +1,58 @@
+import { useEffect, useState } from "react";
 import { MovieDescription } from "../../components/MovieDescription/MovieDescription";
 import { MovieStills } from "../../components/MovieStills/MovieStills";
 import { Recommendations } from "../../components/Recommendations/Recommendations";
 import { TrailerSection } from "../../components/TrailerSection/TrailerSection";
 import { MOCK_MOVIE_DETAILS } from "../../data/DetailsMovie";
 import { MOCK_RECOMMENDATIONS } from "../../data/MovieRecommendations";
+import { getMovieById, getMovies } from "../../api/movies";
+import {mapMovieDetailsFromApi} from "@/types/Movie";
+import { useParams } from "react-router-dom";
 
 export const MovieDetailsPage = () => {
+    const {id} = useParams<{id: string}>();
+    const [movie, setMovie] = useState(MOCK_MOVIE_DETAILS);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        if (!id) {
+            setError('Movie ID is required');
+            setLoading(false);
+            return;
+        }
+
+        const fetchMovies = async () => {
+            try {
+                setLoading(true);
+                const movieData = mapMovieDetailsFromApi(await getMovieById(id));
+                if (movieData) {
+                    setMovie(await movieData);
+                }
+            } catch (err) {
+                setError(err instanceof Error ? err.message : 'Failed to fetch movies');
+                setMovie(MOCK_MOVIE_DETAILS);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchMovies();
+    }, [id]);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        console.warn('Error fetching movies:', error);
+    }
+    
     return (
         <div>
-            <TrailerSection movie={MOCK_MOVIE_DETAILS}/>
-            <MovieDescription movie={MOCK_MOVIE_DETAILS}/>
-            <MovieStills movie={MOCK_MOVIE_DETAILS}/>
+            <TrailerSection movie={movie}/>
+            <MovieDescription movie={movie}/>
+            <MovieStills movie={movie}/>
             <Recommendations recommendations={MOCK_RECOMMENDATIONS}/>
         </div>
     );
