f007cd4724ae417f1703953fdb14e8f92494df4e
Ivan Dariy <dariyvanya633@gmail.com>
2026-02-02 17:13:42 +0200

    Halls Admin (#21)

--- AbsoluteCinema/src/components/layout/AdminSidebar/AdminSidebar.tsx
+++ AbsoluteCinema/src/components/layout/AdminSidebar/AdminSidebar.tsx
@@ -7,7 +7,7 @@ export const AdminSidebar = () => {
         <aside className='admin-sidebar'>
             <nav className='admin-nav'>
                 <button className='admin-nav-btn' onClick={() => navigate('/admin/movies/add')}>Add movies</button>
-                <button className='admin-nav-btn'>Halls</button>
+                <button className='admin-nav-btn' onClick={() => navigate('/admin/halls')}>Halls</button>
                 <button className='admin-nav-btn'>Clients</button>
                 <button className='admin-nav-btn'>Current reservations</button>
             </nav>
