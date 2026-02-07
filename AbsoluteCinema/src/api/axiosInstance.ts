89eaa759ab2c93ee91b0d79a7d30d145afed4dba
Ivan Dariy <dariyvanya633@gmail.com>
2026-02-07 17:10:59 +0200

    fix (#41)

--- AbsoluteCinema/src/api/axiosInstance.ts
+++ AbsoluteCinema/src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.PROD ? 'https://localhost:7191/api' : '/api',
+  baseURL: import.meta.env.PROD ? 'http://localhost:5134/api' : '/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
